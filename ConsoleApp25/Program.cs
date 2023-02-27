d71da9ca6ba616d408ae96bfea3728d572521265
Катерина Буравлева <kkaatteerriinnaa2003@gmail.com>
2023-02-27 22:15:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a + " " + b);
+        }
+        static void Main(string[] args)
+        {
+            int a = 1, b =5;
+            Swap(ref a, ref b);
+
+        }
+    }
+}
